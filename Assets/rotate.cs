3f204719fd5993b9452484305ea1b9425f0531de
auaua159 <63637007+auaua159@users.noreply.github.com>
2021-02-09 18:33:23 +0900

    rotate

---
+++ Assets/rotate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public Transform top;
+    public int t=0;
+    public Vector3 _center = Vector3.zero;
+    public Vector3 _axis = Vector3.up;
+    public float _period = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(
+            _center,
+            _axis,
+            360 / _period * Time.deltaTime
+        );
+    }
+}
