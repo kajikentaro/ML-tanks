440143cabc841b1f7520e0773af52373ddea0920
auaua159 <63637007+auaua159@users.noreply.github.com>
2021-02-09 17:47:32 +0900

    tank

--- Assets/TankControll.cs
+++ Assets/TankControll.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TankControll : MonoBehaviour
-{
+{   
+    /* 
     public float speed = 3f;
 	float moveX = 0f;
 	float moveZ = 0f;
@@ -21,5 +22,23 @@ public class TankControll : MonoBehaviour
  
 	void FixedUpdate(){
 		rb.velocity = new Vector3(moveX, 0, moveZ);
+	}
+    */
+    public float speed = 3.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("down")) {
+			transform.position -= transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("right")) {
+			transform.position += transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("left")) {
+			transform.position -= transform.right * speed * Time.deltaTime;
+		}
 	}
 }
