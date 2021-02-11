c76e3ce02e315da04d7a2b88e44f507b87af7490
saityyy <sec200091@gmail.com>
2021-02-11 19:02:06 +0900

    sound effect

--- Assets/Fx Explosion Pack/Script/effectStart.cs
+++ Assets/Fx Explosion Pack/Script/effectStart.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class effectStart : MonoBehaviour
 {
+
+    public AudioClip sound1;
+    AudioSource audioSource;
+    void Start(){
+        audioSource=GetComponent<AudioSource>();
+    }
     public void startEffect(){
+        audioSource=GetComponent<AudioSource>();
+        audioSource.PlayOneShot(sound1);
         GetComponent<ParticleSystem>().Play();
     }
 }
