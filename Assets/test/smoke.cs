17a55a6ae766e2160965781eea8839125b8eb944
saityyy <sec200091@gmail.com>
2021-02-23 10:04:13 +0900

    set shell effect

---
+++ Assets/test/smoke.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoke : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public float v=0.1f;
+    public int t=0;
+    public int T=500;
+    void Update()
+    {
+        Vector3 pos=transform.forward;
+        pos*=v;
+        transform.position+=pos;
+        if(t%T==0){
+            transform.Rotate(0.0f,90f,0.0f);
+        }
+        t+=1;
+    }
+}
