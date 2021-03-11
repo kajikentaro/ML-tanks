bbe1295f6677c62d741998d5fd8eeeddd923b27a
kajikentaro <k.kentarou0412@gmail.com>
2021-03-11 21:28:42 +0900

    add reycast

--- Assets/MLTank/tankA/step1/MLtankA.cs
+++ Assets/MLTank/tankA/step1/MLtankA.cs
@@ -7,21 +7,19 @@ using Unity.MLAgents.Sensors;
 
 public class MLtankA : MLTank
 {
-    public float endT=0;
-    public float now;
     public float limit_rotation=100.0f;
     public float sum_rotation;
     public GameObject target;
-    float T=0;
+    float start_time;
     public override void Initialize()
     {
     }
     public override void OnEpisodeBegin()
     {
+        start_time = Time.time;
         base.OnEpisodeBegin();
         sum_rotation=0;
         int w=25;
-        T=Time.time;
         transform.localPosition=new Vector3(w*(Random.value-0.5f),0.3f,w*(Random.value-0.5f));
         Vector3 newPosition = new Vector3(w*(Random.value-0.5f),0.3f,w*(Random.value-0.5f));
         float limit = target.transform.localScale.x / 2 * 1.414f + 1.5f;
@@ -34,10 +32,20 @@ public class MLtankA : MLTank
     }
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
+        RaycastHit raycastHit;
+        Physics.Raycast(tankTop.transform.position, tankTop.transform.forward, out raycastHit, 30);
+        if (raycastHit.collider != null && raycastHit.collider.name == "target")
+        {
+            target.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            target.GetComponent<Renderer>().material.color = Color.blue;
+            AddReward(-0.005f);
+        }
+
         action_control(actionBuffers);
-        sum_rotation+=Mathf.Abs(actionBuffers.DiscreteActions[3]-1);
-        now=(Time.time-T)/Time.deltaTime;
-        if(now>endT||launch_cnt>20){
+        if(Time.time - start_time >= 20||launch_cnt>20){
             gameset(-1.0f);
             //gamesetAll();
         }
