70c538b51034f30278d8b59c326f41c3313d8a62
kajikentaro <k.kentarou0412@gmail.com>
2021-05-13 21:06:11 +0900

    complete result.scene

---
+++ Assets/Result/ShareClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[SerializeField]
+public class ShareClick : MonoBehaviour
+{
+    [SerializeField]
+    public void test()
+    {
+        Debug.Log("hello");
+        SocialConnector.SocialConnector.Share("Social Connector", "https://github.com/anchan828/social-connector", null);
+    }
+    public void viewRanking()
+    {
+
+        Application.OpenURL("https://github.com");
+    }
+}
